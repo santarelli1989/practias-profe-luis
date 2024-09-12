e2b7292e795d960d2defb02a434e86c2093db3ab
santarelli1989 <esteban.santarelli@gmail.com>
2024-09-12 11:59:20 -0300

    subiend practicas a la rama develop (metodo reduce)

--- src/app.js
+++ src/app.js
@@ -2,33 +2,39 @@ import chalk from "chalk";
 import data from "./productList.js";
 import table from "./table.js";
 
-/*function chequearStock(stock) {
+const productos = data;
+
+
+function chequearStock(stock) {
   return stock > 20 ? chalk.bgBlue.white(stock) : chalk.bgRed(stock);
-
-
-const info = data.map((producto) => {
+}
+const info = productos.map((producto) => {
   return [
     producto.id,
     producto.nombre,
     producto.precio,
     producto.categoria,
     producto.descripcion,
-    chequearStock (producto.stock)
-
+    chequearStock(producto.stock),
   ];
 });
 table.push(...info);
-
 console.log(table.toString());
-
-*/
-
-const stockBajo = data.filter(
+// metodo filter
+const stockBajo = productos.filter(
 (bajo)=> bajo.stock < 20
 );
-
 console.log (stockBajo);
+// metodo reduce
+ // aca con el metodo reduce sacamos el stock total de todos los productos que tenemos en lista
+const stockTotal = productos.reduce((stock, item)=>{
+ return stock + item.stock
+}, 0)
+console.log (stockTotal)
 
-
-
-
+//metodo reduce
+// sacamos el precio total de la mercaderia
+const precioTotal = productos.reduce((price, item)=>{
+  return   price +  item.precio * item.stock
+}, 0);
+console.log(precioTotal)
